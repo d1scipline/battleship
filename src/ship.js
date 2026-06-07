22d9b9e06c69cfe12967fc51ac6b53fa2cbfed46
d1scipline <143661704+d1scipline@users.noreply.github.com>
2026-06-07 22:53:13 +0300

    Add the rest of the tests for the ship class and implement them

--- src/ship.js
+++ src/ship.js
@@ -1,5 +1,20 @@
 export class Ship {
   constructor(size) {
     this.size = size;
+    this.hits = 0;
+  }
+
+  hit() {
+    if (this.hits < this.size) {
+      this.hits += 1;
+    }
+  }
+
+  isSunk() {
+    if (this.hits == this.size) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
--- src/ship.test.js
+++ src/ship.test.js
@@ -1,8 +1,36 @@
 import { Ship } from "./ship";
 
 test("Ship is constructed correctly", () => {
-  var size = 3;
+  const size = 3;
   const object = new Ship(size);
   expect(object).toBeInstanceOf(Ship);
   expect(object.size).toBe(size);
+  expect(object.hits).toBe(0);
+});
+
+test("Ship gets hit", () => {
+  const size = 3;
+  const object = new Ship(size);
+  object.hit();
+  expect(object.hits).toBe(1);
+});
+
+test("Ship doesn't get hit more than it's size", () => {
+  const size = 2;
+  const object = new Ship(size);
+  object.hit();
+  object.hit();
+  object.hit();
+  expect(object.hits).toBe(2);
+});
+
+test("Ship calculates if it's sunk correctly", () => {
+  const size = 3;
+  const object = new Ship(size);
+  object.hit();
+  expect(object.isSunk()).toBe(false);
+  object.hit();
+  expect(object.isSunk()).toBe(false);
+  object.hit();
+  expect(object.isSunk()).toBe(true);
 });
